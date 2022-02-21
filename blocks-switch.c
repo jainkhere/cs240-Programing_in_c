9a20e5b2ffacc2b6cbc274e6484450bcd39fd504
kunal jain <kkunal95jjain@gmail.com>
2022-02-21 12:40:57 -0500

    switch, break and continue

--- blocks-switch.c
+++ blocks-switch.c
@@ -10,20 +10,6 @@
  */
 
 
-#include<stdio.h>
-
-#define STATE 1
-
-// Macros may not work as expected when
-// defined without brackets.
-#define SQUARENB(A) (A*A)      // Macro without brackets around expression
-#define SQUARE(A) ((A)*(A))    // Macro with brackets around expression 
-
-// Use backslash (\) to write macro definition in two line.
-// Following macro swaps value of x and y. t is data type int.
-#define EXCHG(t, x, y) { t d; d = x; x = y;\
-         y = d;}
-
 // This function shows how switch statement works.
 void test_switch() {
     enum weekdays { ERR, SUN, MON, TUE, WED, THUR, FRI, SAT };
@@ -81,43 +67,6 @@ void test_break_continue() {
     }
 }
 
-// This function shows how to
-// use macros without arguments.
-void test_macro_no_arg() {
-    if (STATE) {
-        printf("Macro substitution works!");
-    }
-}
-
-// This function shows how to 
-// using macro with arguments.
-void test_macro_arg() {
-    int n = 9;
-    n = SQUARENB(n + 1);    // Substitution -> (n + 1*n + 1)
-    printf("Value of n when no brackets in Macro definition -> %d\n", n);
-    n = 9;
-    n = SQUARE(n + 1);      // Substitution -> ((n + 1)*(n + 1))
-    printf("Value of n brackets in Macro definition -> %d\n", n);
-}
-
-// Using EXCHG macro to swap
-// values of two variables.
-void test_macro_exchg() {
-    int a = 5, b = 7;
-    printf("Value of a and be is %d and %d respectively\n", a, b);
-    EXCHG(int, a, b);
-    // Above macro expands as follows
-    // Shown in multiple lines for clarity
-    // {
-    //     int d;
-    //     d = u;
-    //     u = v;
-    //     v = d;    
-    // }
-    // In above expansion, d is defined locally within block.
-    printf("Value of a and be is %d and %d respectively\n", a, b);
-}
-
 int main(void) {
 
     // This is driver function. We call all function
@@ -128,8 +77,7 @@ int main(void) {
 
     // test_switch();
     // test_break_continue();
-    // test_macro_no_arg();
-    // test_macro_arg(); 
-    // test_macro_exchg();
+
+    return 0;
 
 }
