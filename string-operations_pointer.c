d2bb3421a10d2fdb328ce557ccd7d6efc45d03b3
kunal jain <kkunal95jjain@gmail.com>
2022-03-03 09:50:51 -0500

    Template file for string operations using pointers

---
+++ string-operations_pointer.c
@@ -0,0 +1,73 @@
+/**
+ * @file string-operations_pointer.c
+ * @author kunal jain (kunal.jain001@umb.edu)
+ * @brief Program to perform string operations 
+ *        using pointers.
+ * @version 0.1
+ * @date 2022-03-02
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include<stdio.h>
+
+// strlen -> Reads character array one character
+//           at a time and returns its length.
+// Parameter -> character array s[].
+// Returns -> Length of character array.
+
+// First method - for loop with len variable
+int strlen1(char s[]) {
+
+}
+
+// Second method - empty for loop without 
+// extra variable
+int strlen2(char s[]) {
+
+}
+
+// Third method - Empty for loop with pointer
+int strlen3(char* s) {
+
+}
+
+// Fourth method - Empty while loop with pointer
+int strlen4(char* s) {
+
+}
+
+// strcpy -> Copy characters from one
+//           array to another. Here we
+//           will copy t to s.
+// Parameters -> Character array s and t.
+// Returns -> Nothing. Alters array s.
+
+// First method - while loop with char array
+void strcpy1(char s[], char t[]) {
+
+}
+
+// Second method - empty while loop with pointer
+void strcpy2(char* s, char* t) {
+
+}
+
+// strcmp -> Compare two strings. 
+//           Return 0 if they are equal. 
+//           Return 1 if first string appears after second in dictionary.
+//           Return -1 otherwise.
+// Parameters -> Character array s and t.
+// Returns -> Integer. 
+//            0 if s and t equal
+//            1 if s appears after t in dictionary
+//           -1 if t appears after s in dictionary
+int strcmp(char* s, char* t) {
+
+}
+
+int main() {
+
+    return 0;
+}
